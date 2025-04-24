b1bff9a681504c20619a692a27bee419c52c6caa
lintangalf <lintangnindyaalf@gmail.com>
2025-04-24 13:23:05 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,5 +2,5 @@ import streamlit as st
 
 st.title("punya lintaengs")
 st.write(
-    "selamat datang, kepo ya sama isinya. met baca all."
+    "Selamat datang, kepo ya sama isinya. met baca all."
 )
