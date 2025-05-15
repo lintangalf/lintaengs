ef6e216ff19822febd792791f3567b5a24fa18c9
lintangalf <lintangnindyaalf@gmail.com>
2025-05-15 13:37:51 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -5,4 +5,9 @@ st.write(
     "Selamat datang, kepo ya sama isinya. met baca all."
 )
 
-st.image("views/IMG_0417.jpeg")
+st.image("views/IMG_0417.jpeg", width=200)
+st.write("\n")
+st.subheader("Lintang Nindya")
+st.write(
+    "Ayo makan sate taichan sama aku"
+)
